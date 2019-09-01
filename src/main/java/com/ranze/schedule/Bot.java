e964d5fd761d7daf145d55c2877d9df0933df74c
ranze <luyong1107@gmail.com>
2019-09-01 13:52:05 +0800

    Change launch and end action

--- src/main/java/com/ranze/schedule/Bot.java
+++ src/main/java/com/ranze/schedule/Bot.java
@@ -2,6 +2,7 @@ package com.ranze.schedule;
 
 import com.baidu.dueros.bot.BaseBot;
 import com.baidu.dueros.data.request.LaunchRequest;
+import com.baidu.dueros.data.request.SessionEndedRequest;
 import com.baidu.dueros.data.response.OutputSpeech;
 import com.baidu.dueros.data.response.card.TextCard;
 import com.baidu.dueros.model.Response;
@@ -24,16 +25,20 @@ public class Bot extends BaseBot {
         log.info("Launch request: {}", launchRequest.getRequestId());
 
         TextCard textCard = new TextCard("宝贝日程");
-        textCard.addCueWord("你可以创建你的日程");
+        textCard.addCueWord("创建宝贝的日程");
 
         // 新建返回的语音内容
-        OutputSpeech outputSpeech = new OutputSpeech(OutputSpeech.SpeechType.PlainText, "欢迎使用宝贝日程");
+        OutputSpeech outputSpeech = new OutputSpeech(OutputSpeech.SpeechType.PlainText, "欢迎来到宝贝日程");
 
         // 构造返回的Response
-        Response response = new Response(outputSpeech, textCard);
-        return response;
-
+        return new Response(outputSpeech, textCard);
     }
 
+    @Override
+    protected Response onSessionEnded(SessionEndedRequest sessionEndedRequest) {
+        log.info("Session end: {}", sessionEndedRequest.getRequestId());
+        OutputSpeech outputSpeech = new OutputSpeech(OutputSpeech.SpeechType.PlainText, "再见了，记得来打卡哦");
+        return new Response(outputSpeech);
+    }
 
 }
